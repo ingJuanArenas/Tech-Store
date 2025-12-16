75af70e0129969805f1ba1960aa0d4a34ff71a63
ingJuanArenas <juanarenasing@gmail.com>
2025-12-16 09:23:22 -0500

    Repository interface

---
+++ src/main/java/com/tech/domain/repository/ProductsRepository.java
@@ -0,0 +1,18 @@
+package com.tech.domain.repository;
+
+import java.util.List;
+
+import com.tech.domain.dtos.ProductDTO;
+import com.tech.domain.dtos.UpdateDTO;
+import com.tech.persistence.model.Product;
+
+public interface ProductsRepository {
+    List<ProductDTO> getAllCars();
+    ProductDTO getProductById(Long id);
+    List<ProductDTO> getProductsByCategory(String category);
+    List<ProductDTO> searchProducts(String query);
+    ProductDTO createProduct(Product Product);
+    ProductDTO updateProduct(long id, UpdateDTO updateDTO);
+    void deleteProduct(long id);
+
+}
