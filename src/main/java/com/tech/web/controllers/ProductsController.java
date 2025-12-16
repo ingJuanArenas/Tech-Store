12fc1e95a598e5664f9a04421f2cb62c0887d8e9
ingJuanArenas <juanarenasing@gmail.com>
2025-12-16 09:25:08 -0500

    implementing controller

--- src/main/java/com/tech/web/controllers/ProductsController.java
+++ src/main/java/com/tech/web/controllers/ProductsController.java
@@ -3,8 +3,11 @@ package com.tech.web.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,8 +48,8 @@ public class ProductsController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
-        return ResponseEntity.ok(productService.createProduct(productDTO));
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody Product product){
+        return ResponseEntity.ok(productService.createProduct(product));
     }
     @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody UpdateDTO productDTO){
