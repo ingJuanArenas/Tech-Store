2262fb645cd87bb3365cb7c6328f186168736e42
ingJuanArenas <juanarenasing@gmail.com>
2025-12-16 09:22:47 -0500

    Service implementation

--- src/main/java/com/tech/domain/service/ProductService.java
+++ src/main/java/com/tech/domain/service/ProductService.java
@@ -1,8 +1,50 @@
 package com.tech.domain.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tech.domain.dtos.ProductDTO;
+import com.tech.domain.dtos.UpdateDTO;
+import com.tech.domain.repository.ProductsRepository;
+import com.tech.persistence.model.Product;
+
+
+@Service
 public class ProductService {
-    
+    private final ProductsRepository productsRepository;
+
+    public ProductService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        return productsRepository.getAllCars();
+    }
+
+    public ProductDTO getProductById(Long id) {
+        return productsRepository.getProductById(id);
+    }
+    public List<ProductDTO> getProductsByCategory(String category) {
+        return productsRepository.getProductsByCategory(category);
+    }
+
+    public List<ProductDTO> searchProducts(String query) {
+        return productsRepository.searchProducts(query);
+    }
+
+    public ProductDTO createProduct(Product product){
+        return productsRepository.createProduct(product);
+    }
+
+    public ProductDTO updateProduct(long id, UpdateDTO updateDTO) {
+        return productsRepository.updateProduct(id, updateDTO);
+    }
+
+    public void deleteProduct(long id) {
+        productsRepository.deleteProduct(id);
+    }
 }
 
 
-//TODO: get all, get by id, by category, query, create, update, delete
+
