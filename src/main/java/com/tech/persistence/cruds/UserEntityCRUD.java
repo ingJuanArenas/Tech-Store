2239561a946b9451454385c75e436cf105ac24aa
ingJuanArenas <juanarenasing@gmail.com>
2026-03-30 14:42:21 -0500

    findByRole

--- src/main/java/com/tech/persistence/cruds/UserEntityCRUD.java
+++ src/main/java/com/tech/persistence/cruds/UserEntityCRUD.java
@@ -3,13 +3,10 @@ package com.tech.persistence.cruds;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.tech.persistence.model.Role;
 import com.tech.persistence.model.UserEntity;
 
 public interface UserEntityCRUD extends JpaRepository<UserEntity,String> {
-    @Query("SELECT u FROM UserEntity u JOIN u.roles r WHERE r = :role")
-    List<UserEntity> FindByRoles(Role role);
-
+    List<UserEntity> findByRole(Role role);
 }
